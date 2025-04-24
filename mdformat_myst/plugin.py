a8b25c2c8df6419e31b024a3bd581ced917c480a
Nicolas M. Thiéry <nicolas.thiery@universite-paris-saclay.fr>
2025-04-24 16:09:57 +0200

    👌 IMPROVE: Support colon fenced directives

--- mdformat_myst/plugin.py
+++ mdformat_myst/plugin.py
@@ -8,11 +8,69 @@ from mdformat.renderer import RenderContext, RenderTreeNode
 from mdit_py_plugins.dollarmath import dollarmath_plugin
 from mdit_py_plugins.myst_blocks import myst_block_plugin
 from mdit_py_plugins.myst_role import myst_role_plugin
+from mdit_py_plugins.container import container_plugin
 
 from mdformat_myst._directives import fence, render_fence_html
 
 _TARGET_PATTERN = re.compile(r"^\s*\(.+\)=\s*$")
 _ROLE_NAME_PATTERN = re.compile(r"({[a-zA-Z0-9_\-+:]+})")
+_YAML_HEADER_PATTERN = re.compile(r"(?m)(^:\w+: .*\n)+|^---$\n(?s:.).*\n---\n")
+
+container_names = [
+    "admonition",
+    "attention",
+    "caution",
+    "danger",
+    "div",
+    "dropdown",
+    "embed",
+    "error",
+    "exercise",
+    "exercise-end",
+    "exercise-start",
+    "figure",
+    "glossary",
+    "grid",
+    "grid-item",
+    "grid-item-card",
+    "hint",
+    "image",
+    "important",
+    "include",
+    "index",
+    "literal-include",
+    "margin",
+    "math",
+    "note",
+    "prf:algorithm",
+    "prf:assumption",
+    "prf:axiom",
+    "prf:conjecture",
+    "prf:corollary",
+    "prf:criterion",
+    "prf:definition",
+    "prf:example",
+    "prf:lemma",
+    "prf:observation",
+    "prf:proof",
+    "prf:property",
+    "prf:proposition",
+    "prf:remark",
+    "prf:theorem",
+    "seealso",
+    "show-index",
+    "sidebar",
+    "solution",
+    "solution-end",
+    "solution-start",
+    "span",
+    "tab-item",
+    "tab-set",
+    "table",
+    "tip",
+    "topics",
+    "warning",
+]
 
 
 def update_mdit(mdit: MarkdownIt) -> None:
@@ -50,6 +108,39 @@ def update_mdit(mdit: MarkdownIt) -> None:
     # CommonMark AST, so we need to do this to make validation pass.
     mdit.add_render_rule("fence", render_fence_html)
     mdit.add_render_rule("code_block", render_fence_html)
+
+    for name in container_names:
+        container_plugin(mdit, name="{" + name + "}", marker=":")
+
+
+def container_renderer(
+    node: RenderTreeNode, context: RenderContext, *args, **kwargs
+) -> str:
+    result = node.markup + node.info + "\n"
+    children = node.children
+    if children:
+        # Look at the tokens forming the first paragraph and see if
+        # they form a YAML header. This could be stricter: there
+        # should be exactly three tokens: paragraph start, YAML
+        # header, paragraph end.
+        tokens = children[0].to_tokens()
+        if all(
+            not token.content or _YAML_HEADER_PATTERN.fullmatch(token.content)
+            for token in tokens
+        ):
+            # If yes, render these tokens as is
+            for token in tokens:
+                if token.content:
+                    result += token.content + "\n"
+            result += "\n"
+            # and skip that first paragraph
+            children = children[1:]
+
+    result += (
+        "\n\n".join(child.render(context) for child in children) + "\n" + node.markup
+    )
+
+    return result
 
 
 def _role_renderer(node: RenderTreeNode, context: RenderContext) -> str:
@@ -130,4 +221,9 @@ RENDERERS = {
     "math_block": _math_block_renderer,
     "fence": fence,
 }
+
+
+for name in container_names:
+    RENDERERS["container_{" + name + "}"] = container_renderer
+
 POSTPROCESSORS = {"paragraph": _escape_paragraph, "text": _escape_text}
--- tests/data/fixtures.md
+++ tests/data/fixtures.md
@@ -406,3 +406,86 @@ MyST directive, no opts or content
 ```{some-directive} args
 ```
 .
+
+:::{admonition} MyST colon fenced directive with a title
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+:::
+
+.
+
+:::{admonition} MyST colon fenced directive with simple metadata
+:class: foo :truc: bla
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+:::
+
+% Admonitions with arbitrary yaml metadata are not yet supported.
+% Issue: in a container, the `---` is interpreted as hrule by the parser
+%
+% :::{admonition} MyST colon fenced directive with arbitrary yaml metadata
+% ---
+% foo:
+%   bar: 1
+% ---
+%
+% Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+% incididunt ut labore et dolore magna aliqua.
+% :::
+
+.
+
+% Unknown colon-fenced directives are not yet implemented
+% :::{exercise}
+% This is an unknown admonition.
+% :::
+
+.
+
+::::{admonition} MyST colon fenced directive with two nested admonitions
+:::{admonition}
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+:::
+
+:::{admonition}
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+:::
+
+:::{admonition}
+truc
+:::
+::::
+
+.
+
+::::{hint} A hint with alternating nested tips and texts
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+
+:::{tip}
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+:::
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+
+:::{tip}
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+:::
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+::::
+
+.
+
+- foo
+  :::{tip} A directive nested in bullet points
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+  incididunt ut labore et dolore magna aliqua.
+  :::
